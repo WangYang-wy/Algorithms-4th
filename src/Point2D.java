857f435dd46097f7b8050d9cc1079dd8a58d0721
WangYang-wy <1070942929@qq.com>
2018-02-05 16:23:33 +0800

    add handouts

---
+++ src/Point2D.java
@@ -0,0 +1,39 @@
+import edu.princeton.cs.algs4.Stack;
+import org.jetbrains.annotations.Contract;
+
+import java.util.Arrays;
+
+public class Point2D {
+    private final double x;
+    private final double y;
+
+    /**
+     * constructor.
+     *
+     * @param x x.
+     * @param y y.
+     */
+    public Point2D(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Contract(pure = true)
+    public static int ccw(Point2D a, Point2D b, Point2D c) {
+        double area_2 = (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x);
+
+        if (area_2 < 0) {
+            return -1;
+        } else if (area_2 > 0) {
+            return 1;
+        } else {
+            return 0; // collinear.
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack<Point2D> hull = new Stack<Point2D>();
+
+//        Arrays.sort();
+    }
+}
